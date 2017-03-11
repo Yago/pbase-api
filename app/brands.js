f740c1b2965e74bc62c9dc03cf2325b72ff6c771
Yann Gouffon <github@yago-art.ch>
2017-03-11 12:34:56 +0100

    ✨ add brands route

---
+++ app/brands.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+module.exports = async (req, res) => {
+  const brand = req.params.brand;
+  const url = `http://www.pbase.com/cameras/`;
+
+  const dom = await axios.get(url);
+  const $ = cheerio.load(dom.data);
+  const brands = [];
+
+  $('td').each(function(i, elem) {
+    const brandUrl = $(this).find('a').attr('href');
+    const brand = brandUrl ? brandUrl.replace('/cameras/', '') : null;
+
+    if (brand && brand.indexOf('/') === -1) {
+      brands.push(brand);
+    }
+  });
+
+  const response = brands
+    .sort((a, b) => {
+      if(a < b) return -1;
+      if(a > b) return 1;
+     return 0;
+    })
+    .reduce((acc, value) => {
+      if (acc.indexOf(value) === -1) {
+        acc.push(value);
+        console.log(value);
+      }
+      return acc;
+    }, []);
+
+  res.send(response);
+};
--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,7 @@
+const brands = require('./brands');
 const items = require('./items');
 
 module.exports = (app) => {
   app.get('/items/:brand', items);
+  app.get('/brands', brands);
 };
