f4f08247e31921993df9dae00de0591806f7448c
Yann Gouffon <github@yago-art.ch>
2017-03-15 15:09:30 +0100

    🐛 fix heroku delpoy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app  = express();
-const port = 80;
+const port = process.env.PORT || 8080;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
